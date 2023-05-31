25cc127506e44ea2ade4d0c139fedb9690ee16b5
thiago.machado <thiago.machado@seati.ma.gov.br>
2023-05-30 23:52:30 -0300

    feat: implementando rouing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './pages/content/content.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'content/:id',
+    component:ContentComponent
+  }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
